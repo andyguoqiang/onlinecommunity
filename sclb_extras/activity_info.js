cd571443abbf41d4b535a8a7a89f15ad85adb449
ac <andyguoqiang@hotmail.com>
2022-05-24 07:31:59 -0400

    First Commit

---
+++ sclb_extras/activity_info.js
@@ -0,0 +1,34 @@
+/**
+ *  ==================================================
+ *  SoftChalk LessonBuilder
+ *  Activity activity_info.js
+ *  Copyright 2008 SoftChalk LLC
+ *  All Rights Reserved.
+ *
+ *  http://www.softchalk.com
+ *  ==================================================
+ */
+
+q_done_a=new Array();
+scoreQa=new Array();
+score_a=new Array();
+show_restart_a=new Array();
+num_pages=7;
+actOrder=new Array(2);
+actOrder[0]=9;
+actOrder[1]=11;
+
+//a_num=9;
+a_value9=0;
+scoreQa[9]=false;
+q_done_a[9]=false;
+score_a[9]=0;
+show_restart_a[9]=true;
+
+//a_num=11;
+a_value11=0;
+scoreQa[11]=false;
+q_done_a[11]=false;
+score_a[11]=0;
+show_restart_a[11]=true;
+
